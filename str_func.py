af96f9c4dbd26f25147d9433dc602ad3833b8923
Vlad Morozov <v@morosoff.ru>
2023-11-03 23:01:12 +0300

    add get_cap_str function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def get_up_str(string):
+    """принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     return string.upper()
+
+
+def get_cap_str(string):
+    """делает заглавными первые буквы каждого слова в строке"""
+    return string.capitalize()
